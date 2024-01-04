33abf19bbfccc309ec1d97905a156e205fd441fc
ᴷʳᵉˢˢʷᵉˡˡ <147948046+EscaliBud@users.noreply.github.com>
2024-01-04 16:35:59 +0300

    Update escalibud.js

--- escalibud.js
+++ escalibud.js
@@ -440,18 +440,18 @@ case 'weather':{
 if (!text) return reply('What location?')
 const kressweather="https://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en";
             let wdata = await axios.get(kressweather);
-            let textw = ""
-            textw += *🗺️Weather of  ${text}*\n\n
-            textw += *Weather:-* ${wdata.data.weather[0].main}\n
-            textw += *Description:-* ${wdata.data.weather[0].description}\n
-            textw += *Avg Temp:-* ${wdata.data.main.temp}\n
-            textw += *Feels Like:-* ${wdata.data.main.feels_like}\n
-            textw += *Pressure:-* ${wdata.data.main.pressure}\n
-            textw += *Humidity:-* ${wdata.data.main.humidity}\n
-            textw += *Humidity:-* ${wdata.data.wind.speed}\n
-            textw += *Latitude:-* ${wdata.data.coord.lat}\n
-            textw += *Longitude:-* ${wdata.data.coord.lon}\n
-            textw += *Country:-* ${wdata.data.sys.country}\n
+            let textw = `
+           *🗺️Weather of  ${text}*\n\n
+           *Weather:-* ${wdata.data.weather[0].main}\n
+           *Description:-* ${wdata.data.weather[0].description}\n
+           *Avg Temp:-* ${wdata.data.main.temp}\n
+           *Feels Like:-* ${wdata.data.main.feels_like}\n
+           *Pressure:-* ${wdata.data.main.pressure}\n
+           *Humidity:-* ${wdata.data.main.humidity}\n
+           *Humidity:-* ${wdata.data.wind.speed}\n
+           *Latitude:-* ${wdata.data.coord.lat}\n
+           *Longitude:-* ${wdata.data.coord.lon}\n
+           *Country:-* ${wdata.data.sys.country}\n`;
 
            client.sendMessage(
                 m.chat, {
