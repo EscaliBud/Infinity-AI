9e5a4a67183b5db37e1f51ce0126dcbb51653fae
Kresswell <130984721+Kresswell@users.noreply.github.com>
2024-01-09 13:24:25 +0300

    Update escalibud.js

--- escalibud.js
+++ escalibud.js
@@ -509,24 +509,6 @@ const rel = await quote(xf, pushname, pppuser)
             }
 
 break;
-case 'apk':
-case 'apkdl':{
-if (!text) return reply("What apk u wanna download?")
-let resMaria = await fetch(`https://vihangayt.me/download/apk?id=${text}`)
-let jsonMaria = await resMaria.json()
-client.sendMessage(from, { document: { url: jsonMaria.data.dllink}, fileName : jsonMaria.data.name, mimetype: 'application/vnd.android.package-archive'}, {quoted:m})
-.catch(console.error)
-}
-break;
-case 'apk':
-case 'apkdl':{
-if (!text) return reply("What apk u wanna download?")
-let resMaria = await fetch(`https://vihangayt.me/download/apk?id=${text}`)
-let jsonMaria = await resMaria.json()
-client.sendMessage(from, { document: { url: jsonMaria.data.dllink}, fileName : jsonMaria.data.name, mimetype: 'application/vnd.android.package-archive'}, {quoted:m})
-.catch(console.error)
-}
-break;
 
 case 'apk':
 case 'apkdl':{
