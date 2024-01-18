c97a0bbd5533591fde08970b9d246ab324fdc606
Kresswell <130984721+Kresswell@users.noreply.github.com>
2024-01-18 16:04:47 +0300

    Update escalibud.js

--- escalibud.js
+++ escalibud.js
@@ -888,6 +888,60 @@ case 'public': {
                 reply('*Successful in Changing To Public Usage*')
             }
             break;
+case 'true':
+case 'truecaller':
+  try {
+    if (!text) {
+      // Reply when no phone number is provided
+    return  reply ('Please provide a phone number.');
+      break;
+    }
+
+    const installationId = 'a1i0g--k3toNiVP-9swCenahQhhokTiqfXRFw2LossLOsZLDh3P-fLD0b75S8iF7';
+    const apiUrl = `https://sid-bhai.vercel.app/api/truecaller?phone=${encodeURIComponent(text)}&id=${installationId}`;
+
+    let response = await axios.get(apiUrl);
+    console.log(response);
+    let json = response.data;
+
+    const { name, alternateName, addresses, email, countryDetails } = json;
+
+    let info = `╭––『 *Phone Detail* 』\n`;
+    info += `┆ ⚝ *Name:* ${name}\n`;
+
+    if (addresses && addresses.length > 0) {
+      info += `┆ ⚝ *Address:* ${addresses[0].city}, ${addresses[0].countryCode}\n`;
+      info += `┆ ⚝ *Time Zone:* ${addresses[0].timeZone}\n`;
+      info += `┆ ⚝ *Pin Code* ${addresses[0].zipCode}\n`;
+      info += `┆ ⚝ *Street* ${addresses[0].street}\n`;
+    }
+
+    info += `┆ ⚝ *Email:* ${email}\n`;
+    info += `╰–––––––––––––––༓\n`;
+
+    if (countryDetails) {
+      info += `╭––『 *countryDetails* 』\n`;
+      info += `┆ ⚝ *Name:* ${countryDetails.name}\n`;
+      info += `┆ ⚝ *Native:* ${countryDetails.native}\n`;
+      info += `┆ ⚝ *Phone Code:* +${countryDetails.phone[0]}\n`;
+      info += `┆ ⚝ *Continent:* ${countryDetails.continent}\n`;
+      info += `┆ ⚝ *Capital:* ${countryDetails.capital}\n`;
+      info += `┆ ⚝ *Currency:* ${countryDetails.currency.join(', ')}\n`;
+      info += `┆ ⚝ *Languages:* ${countryDetails.languages.join(', ')}\n`;
+      info += `┆ ⚝ *Flag:* ${countryDetails.flag}\n`;
+      info += `╰–––––––––––––––༓`;
+    }
+
+    await client.sendMessage(m.chat, {
+      text: info,
+    }, {
+      quoted: m,
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+  break;
 case 'tempmail':
     const option = ['.mail 1','.mail 3','.mail 5'];
     client.sendPoll(m.chat, 'Select your mail:', option);
